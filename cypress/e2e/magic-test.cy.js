bbf86cf0f19acf82ec5ee48bd4ed7b469ac34a01
Mira <55487177+mira-osd@users.noreply.github.com>
2022-06-12 21:13:26 +0200

    [optimize] update cypress test

--- cypress/e2e/magic-test.cy.js
+++ cypress/e2e/magic-test.cy.js
@@ -1,18 +1,21 @@
 describe('empty spec', () => {
-  
-  it('passes', () => {
-    // cy.visit('http://localhost:3000/')
-    // cy.visit('http://localhost:3000/gallery')
-    // cy.get("a").each($el => {
-    //   if ($el.prop('href').length > 0) {
-    //         cy.visit("http://localhost:3000"+$el.attr('href'))         
-    //         cy.go("back")
-    //   }
-    // })
+
+  it('testing the opening of gallery and individual card', () => {
+    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:3000/gallery')
+    cy.get("a").each($el => {
+      if ($el.prop('href').length > 0) {
+            cy.visit("http://localhost:3000"+$el.attr('href'))         
+            cy.go("back")
+      }
+    })
+  })
+
+  it('testing the form', () => {
     cy.visit('http://localhost:3000/contact')
     cy.get('#email').type('eemi@eemi.com')
-    cy.get('#name').type('John Doe')
+    cy.get('#name_input').type('John Doe')
     cy.get('#message').type('test')
-    cy.get('button').click()
+    cy.get('button').click({ multiple: true })
   })
 })
